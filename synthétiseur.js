c32c0749599c1484f15047fe3e6df39e4f0a04ee
FIHNEZIUGFIUQRGBSIQBRG <124518301+FIHNEZIUGFIUQRGBSIQBRG@users.noreply.github.com>
2023-02-05 21:15:52 +0100

    Update synthétiseur.js

--- synthétiseur.js
+++ synthétiseur.js
@@ -49,5 +49,8 @@ app.get('/', (req, res) => {
     }
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log(Application démarrée sur le port ${port});
+});
 
